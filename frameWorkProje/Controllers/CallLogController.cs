f9478133af16466eabb9bcf7a405c9c2282c3661
atakanHim <atakanhim@gmail.com>
2022-07-20 15:02:11 +0300

    update atabilecek

--- frameWorkProje/Controllers/CallLogController.cs
+++ frameWorkProje/Controllers/CallLogController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concreate;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concreate;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,17 +11,30 @@ namespace frameWorkProje.Controllers
 {
     public class CallLogController : Controller
     {
+        CallLogManager callLogManager = new CallLogManager(new EfCallLogRepository());
         // GET: CallLog
         public ActionResult Index(int id = 0)
         {
-            CallLogManager callLogManager = new CallLogManager(new EfCallLogRepository());
-            var model = callLogManager.GetAllList();
-            // cagrı detay sayfasu
-            if (id > 0)
+            if (id <= 0)
             {
-                model = callLogManager.GetCallLogListVithFilter(x => x.CallLogId == id);
+                return RedirectToAction("Index","Home");
             }
-
+            var model = callLogManager.GetById(id);
+           
             return View(model);
         }
-  }  }
+        [HttpPost]
+        public ActionResult Index(CallLog call)
+        {
+   // veri geliyor
+
+            return View();
+        }
+        public ActionResult CallLogDetay(int id = 0)
+        {
+           
+          
+            return View();
+           
+        }
+    }  }
