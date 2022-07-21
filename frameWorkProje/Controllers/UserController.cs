389628538cab50399b8436be87bc9b9c60487746
atakanHim <atakanhim@gmail.com>
2022-07-21 17:39:09 +0300

    personel tarafından tamamlanan işlerde gösterildi.

--- frameWorkProje/Controllers/UserController.cs
+++ frameWorkProje/Controllers/UserController.cs
@@ -27,17 +27,26 @@ namespace frameWorkProje.Controllers
         public ActionResult ShowRapor(int id=0,string deger="")//user id aldık
         {
             var jobs = new List<Job>();
-
+            
             if (deger != "")
             {
+                var tamamlananis = 0;
+
                 int gun = Convert.ToInt32(deger);
                  jobs = c.Jobs.SqlQuery("select * from Jobs where userId='" + id + "' and  CreatingTime between Convert(date,'" + DateTime.Now.AddDays(-gun) + "',104) " +
              " and Convert(date,'" + DateTime.Now + "',104) ").ToList();
+                foreach (var item in jobs)
+                {
+                    if(item.JobStatus=="deaktif")
+                        tamamlananis++;
+                }
+
+                ViewBag.count = jobs.Count;
+                ViewBag.tamamlananis = tamamlananis;
                 return View(jobs);
             }
 
-            // bu id nin aldığı işleri listeleyeceğiz
-            //  var jobs = c.Jobs.SqlQuery("select * from Jobs where userId='" + id + "' and CreatingTime=Convert(date,'" + DateTime.Now.ToShortDateString()+ "',104)").ToList();
+            
 
             return View();
       
