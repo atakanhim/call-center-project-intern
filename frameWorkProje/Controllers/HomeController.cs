f9993d7024064517f7c3d514425e2175930680d7
atakanHim <atakanhim@gmail.com>
2022-07-22 16:12:53 +0300

    tamam sorun çözüldü. callog controlerda bunu adminde çagırrız dediğimiz ksıımda else donunce direk home indexe dönüyordu.

--- frameWorkProje/Controllers/HomeController.cs
+++ frameWorkProje/Controllers/HomeController.cs
@@ -15,11 +15,30 @@ namespace frameWorkProje.Controllers
     public class HomeController : Controller
     {
         JobManager jm = new JobManager(new EfJobRepository());
-        public ActionResult Index()
+        public ActionResult Index(string ad = "", int? numara = null, string siralama = "")
         {
 
-            var values = jm.GetList();
-            return View(values);
+            if (ad == "" && numara == null && siralama == "")
+            {
+
+                if (isAdmin())
+                {
+                    var values = jm.GetList();
+                    return View(values);
+                }
+                else
+                {
+                    var values2 = jm.GetJobWithfilter("", null, "", isAdmin(), Convert.ToInt32(Session["userId"]));
+                    return View(values2);
+                }
+
+            }
+
+            else
+            {
+                var values = jm.GetJobWithfilter(ad, numara, siralama, isAdmin(), Convert.ToInt32(Session["userId"]));
+                return View(values);
+            }
         }
         public ActionResult IndexDataTable(string ad = "", int? numara = null, string siralama = "")
         {
