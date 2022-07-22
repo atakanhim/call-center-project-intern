6618a5e4d3d6d2d23330deb99284e8a26ca69dfd
atakanHim <atakanhim@gmail.com>
2022-07-22 16:38:43 +0300

    a

--- frameWorkProje/Controllers/HomeController.cs
+++ frameWorkProje/Controllers/HomeController.cs
@@ -17,28 +17,19 @@ namespace frameWorkProje.Controllers
         JobManager jm = new JobManager(new EfJobRepository());
         public ActionResult Index(string ad = "", int? numara = null, string siralama = "")
         {
-
-            if (ad == "" && numara == null && siralama == "")
+            var values = jm.GetJobWithfilter(ad, numara, siralama, isAdmin(), Convert.ToInt32(Session["userId"]));
+            if (isAdmin())
             {
-
-                if (isAdmin())
-                {
-                    var values = jm.GetList();
-                    return View(values);
-                }
-                else
-                {
-                    var values2 = jm.GetJobWithfilter("", null, "", isAdmin(), Convert.ToInt32(Session["userId"]));
-                    return View(values2);
-                }
-
-            }
-
-            else
-            {
-                var values = jm.GetJobWithfilter(ad, numara, siralama, isAdmin(), Convert.ToInt32(Session["userId"]));
+                values = jm.GetList();
                 return View(values);
             }
+            else
+            {
+                var values2 = jm.GetJobWithfilter("", null, "", isAdmin(), Convert.ToInt32(Session["userId"]));
+                return View(values2);
+            }
+     
+
         }
         public ActionResult IndexDataTable(string ad = "", int? numara = null, string siralama = "")
         {
@@ -66,7 +57,7 @@ namespace frameWorkProje.Controllers
             }
 
         }
-       
+
         public bool isAdmin()
         {
             if (Convert.ToInt32(Session["roleId"]) == 2)
